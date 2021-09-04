c648d53e1ed00361f7c2526f6ad5fad6744172ee
zjtomoon <zjtomoon@outlook.com>
2021-09-04 10:03:26 +0800

    猜随机数游戏

--- rust-prog-lang/random_guess/src/main.rs
+++ rust-prog-lang/random_guess/src/main.rs
@@ -3,7 +3,7 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-	println!("Guess the number !");
+/* 	println!("Guess the number !");
 	let secret_number = rand::thread_rng().gen_range(1,101);
 	let mut guess = String::new();
 	io::stdin().read_line(&mut guess).expect("Failed to read line");
@@ -13,5 +13,31 @@ fn main() {
 		Ordering::Less => println!("Too small!"),
 		Ordering::Greater => println!("Too big!"),
 		Ordering::Equal => println!("You win!"),
+	} */
+
+	println!("Guess the number !");
+	let secret_number = rand::thread_rng().gen_range(1,101);
+
+	loop {
+		println!("Please input your guess.");
+
+		let mut guess = String::new();
+
+		io::stdin().read_line(&mut guess).expect("Failed to read line");
+		let guess: u32 = match guess.trim().parse() {
+			Ok(num) => num,
+			Err(_) => continue,
+		};
+
+		println!("You guessed : {}",guess);
+
+		match guess.cmp(&secret_number) {
+			Ordering::Less => println!("Too small!"),
+			Ordering::Greater => println!("Too big!"),
+			Ordering::Equal => {
+				println!("You win !");
+				break;
+			}
+		}
 	}
 }
