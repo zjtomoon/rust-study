ff3b769dd923847f2609bbe08f6b116a70d57b7d
zjtomoon <zjtomoon@outlook.com>
2021-11-02 14:39:38 +0800

    default commit

--- rust-prog-lang/chapter12/minigrep/src/main.rs
+++ rust-prog-lang/chapter12/minigrep/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
 
 fn main() {
 
@@ -11,8 +11,9 @@ fn main() {
     //println!("{:?}",args);
     //let (query,filename) = parse_config(&args);
     //let config = Config::new(&args);
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}",err);
+    let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
+        //println!("Problem parsing arguments: {}",err);
+        eprintln!("Problem parsing arguments: {}",err);
         process::exit(1);
     });
     //let query = &args[1];
@@ -21,8 +22,9 @@ fn main() {
     println!("Searching for {}",config.query);
     println!("In file {}",config.filename);
 
-    if let Err(e) = run(config) {
-        println!("Application error : {}",e);
+    if let Err(e) = minigrep::run(config) {
+        //println!("Application error : {}",e);
+        eprintln!("Application error: {}",e);
 
         process::exit(1);
     }
@@ -40,14 +42,7 @@ fn main() {
 
     println!("With text:\n {}",contents);
 }*/
-fn run(config: Config) -> Result<(),Box<dyn Error>> {
 
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n {}",contents);
-
-    Ok(())
-}
 
 /*fn parse_config(args: &[String]) -> (&str,&str) {
     let query = &args[1];
@@ -55,24 +50,7 @@ fn run(config: Config) -> Result<(),Box<dyn Error>> {
 
     (query,filename)
 }*/
-struct Config {
-    query: String,
-    filename: String,
-}
-impl Config {
-    fn new(args: &[String]) -> Result<Config,&'static str>{
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
 
-        Ok(Config {
-            query,
-            filename
-        })
-    }
-}
 
 /*fn parse_config(args: &[String]) -> Config {
     let query = args[1].clone();
