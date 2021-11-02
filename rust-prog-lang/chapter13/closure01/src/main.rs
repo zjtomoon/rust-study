ac02ddf14d82a395fffc70834948b48ddcf852a1
zjtomoon <zjtomoon@outlook.com>
2021-11-02 17:44:08 +0800

    default commit

--- rust-prog-lang/chapter13/closure01/src/main.rs
+++ rust-prog-lang/chapter13/closure01/src/main.rs
@@ -1,8 +1,10 @@
 use std::thread;
 use std::time::Duration;
 
+//模拟健身计划
+
 fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("calculating slowingly...");
+    println!("calculating slowly...");
     thread::sleep(Duration::from_secs(2));
     intensity
 }
@@ -19,25 +21,32 @@ fn main() {
 
 fn generate_workout(intensity: u32,random_number: u32) {
     //let expensive_result = simulated_expensive_calculation(intensity);
-    let expensive_closure = |num: u32| -> u32 {
+    /*let expensive_closure = |num: u32| -> u32 {
         println!("calculating slowingly...");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    };*/
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
 
     if intensity < 25 {
         println!(
             "Today, do {} pushups!",
             //simulated_expensive_calculation(intensity)
             //expensive_result
-            expensive_closure(intensity)
+            //expensive_closure(intensity)
+            expensive_result.value(intensity)
         );
         println!(
             "Next, do {} situps",
 
             //simulated_expensive_calculation(intensity)
             //expensive_result
-            expensive_closure(intensity)
+            //expensive_closure(intensity)
+            expensive_result.value(intensity)
         );
     } else {
         if random_number == 3 {
@@ -47,9 +56,35 @@ fn generate_workout(intensity: u32,random_number: u32) {
                 "Today,run for {} minutes",
                 //simulated_expensive_calculation(intensity)
                 //expensive_result
-                expensive_closure(intensity)
+                //expensive_closure(intensity)
+                expensive_result.value(intensity)
             );
 
+        }
+    }
+}
+
+struct Cacher<T> where T: Fn(u32) -> u32 {
+    calculation: T,
+    value: Option<u32>
+}
+
+impl<T> Cacher<T> where T: Fn(u32) -> u32 {
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self,arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation) (arg);
+                self.value = Some(v);
+                v
+            },
         }
     }
 }
