43a23af41536f78a5dd26507d793b27d65fb1505
zjtomoon <zjtomoon@outlook.com>
2021-11-17 10:54:01 +0800

    default commit

--- rust-prog-lang/chapter16/channel_mpsc/src/main.rs
+++ rust-prog-lang/chapter16/channel_mpsc/src/main.rs
@@ -1,13 +1,55 @@
-use std::sync::mpsc;
 use std::thread;
+use std::sync::mpsc;
+use std::time::Duration;
+
 fn main() {
     let (tx,rx) = mpsc::channel();
 
-    thread::spawn(move || {
+    //使用消息传递在线程间转移数据
+   /* thread::spawn(move || {
        let val = String::from("hi");
         tx.send(val).unwrap();
+        //println!("val is {}",val); //将val发送给通道后再尝试使用它
     });
 
     let received = rx.recv().unwrap();
-    println!("Got :{}",received);
+    println!("Got :{}",received);*/
+
+    //发送多个值并观察接收者的等待过程
+    //通过克隆发送者创建多个生产者
+    let tx1 = mpsc::Sender::clone(&tx);
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            //tx.send(val).unwrap();
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    //通过克隆发送者创建多个生产者
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}",received);
+    }
 }
