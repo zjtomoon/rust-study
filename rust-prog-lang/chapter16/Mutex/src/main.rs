eed77de49013da778a39ce007ae62cbb10fdaf62
zjtomoon <zjtomoon@outlook.com>
2021-11-17 15:22:21 +0800

    default commit

--- rust-prog-lang/chapter16/Mutex/src/main.rs
+++ rust-prog-lang/chapter16/Mutex/src/main.rs
@@ -1,4 +1,5 @@
-use std::sync::Mutex;
+//use std::rc::Rc;
+use std::sync::{Mutex,Arc};
 use std::thread;
 
 fn main() {
@@ -10,17 +11,27 @@ fn main() {
     }
 
     println!("m = {:?}",m);*/
-
-    let counter = Mutex::new(0);
+    //尝试使用Rc<T>来允许多个线程持有Mutex<T>
+    //let counter = Rc::new(Mutex::new(0));
+    //let counter = Mutex::new(0);
+    let counter = Arc::new(Mutex::new(0));
     let mut handles = vec![];
 
     for _ in 0..10 {
-        let handle = thread::spawn(|| {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
             *num += 1;
         });
 
         handles.push(handle);
+/*
+        let handle2 = thread::spawn(move || {
+            let mut num2 = counter.lock().unwrap();
+
+            *num2 += 1;
+        });
+        handles.push(handle2);*/
     }
 
     for handle in handles {
