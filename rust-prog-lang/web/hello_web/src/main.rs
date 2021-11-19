79d21b18c2b8cca3a67bab6f97cf82b0820d471f
zjtomoon <zjtomoon@outlook.com>
2021-11-19 15:23:36 +0800

    default commit

---
+++ rust-prog-lang/web/hello_web/src/main.rs
@@ -0,0 +1,58 @@
+use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::fs;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        //println!("Connection established !");
+
+        handle_connection(stream);
+
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"Get / HTTP:1.1\r\n";
+
+    //let response = "HTTP/1.1 200 OK\r\n\r\n";
+/*
+    if buffer.starts_with(get) {
+
+        let contents = fs::read_to_string("hello.html").unwrap();
+
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}",contents);
+
+        stream.write(response.as_bytes()).unwrap();
+
+        stream.flush().unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let response = format!("{} {}",status_line,contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }*/
+    let (status_line,filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n","hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n","404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{} {}",status_line,contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+    println!("Request: {}",String::from_utf8_lossy(&buffer[..]));
+}
