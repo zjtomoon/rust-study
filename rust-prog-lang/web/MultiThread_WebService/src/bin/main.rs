a570896dc274d49741959fdb4aab628ecb67f2ce
zjtomoon <zjtomoon@outlook.com>
2021-11-19 16:19:08 +0800

    default commit

--- rust-prog-lang/web/MultiThread_WebService/src/bin/main.rs
+++ rust-prog-lang/web/MultiThread_WebService/src/bin/main.rs
@@ -1,18 +1,24 @@
+use std::thread;
+use std::time::Duration;
 use std::net::TcpListener;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::fs;
+use std::thread::Thread;
+use MultiThread_WebService::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         //println!("Connection established !");
 
-        handle_connection(stream);
-
+/*        thread::spawn(|| {
+            handle_connection(stream);
+        });*/
     }
 }
 
@@ -22,6 +28,7 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     //let response = "HTTP/1.1 200 OK\r\n\r\n";
 /*
@@ -43,6 +50,9 @@ fn handle_connection(mut stream: TcpStream) {
         stream.flush().unwrap();
     }*/
     let (status_line,filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n","hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n","hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n","404.html")
