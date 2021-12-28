70de651cdf1d01341154cc95278619f0021682eb
zjtomoon <zjtomoon@outlook.com>
2021-12-28 15:15:39 +0800

    rust匹配模式

---
+++ rust-prog-lang/chapter18/enum_match02/src/main.rs
@@ -0,0 +1,37 @@
+enum Color {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(Color),
+}
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+fn main() {
+    //解构嵌套的结构体和枚举
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+    match msg {
+        Message::ChangeColor(Color::Rgb(r, g, b)) => {
+            println!("Change the color to read {},green {}, and blue {}", r, g, b)
+        }
+        Message::ChangeColor(Color::Hsv(h, s, v)) => {
+            println!(
+                "Change the color to hue {}, saturation {}, and value {}",
+                h, s, v
+            )
+        }
+        _ => (),
+    }
+
+    //解构结构体和元组
+    let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+}
