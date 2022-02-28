dbea1ad573167441c60182820a25287f9b2b9e9b
zjtomoon <zjtomoon@outlook.com>
2022-02-28 16:50:13 +0800

    rust实时天气命令行工具

--- atom_demos/weather-cli/src/main.rs
+++ atom_demos/weather-cli/src/main.rs
@@ -1,23 +1,30 @@
 use exitfailure::ExitFailure;
+mod weather;
 use structopt::StructOpt;
-use reqwest::Url;
-use serde_derive::{Deserialize, Serialize};
+use weather::W;
 
 #[derive(StructOpt)]
 struct Input {
     city: String
 }
-
 #[tokio::main]
-async fn main() -> Result<(), ExitFailure> {
+async fn main() -> Result<(),ExitFailure>{
     let input = Input::from_args();
-    println!("{}", input.city);
+    let resp = W::get(&input.city).await?;
+
+    // TODO: 绝对温度转摄氏度
+    println!("{} \n 当前温度： {} °C\n 最高温度： {} °C\n 最低温度： {} °C\n 湿度： {}\n 大气压: {} hPa\n 体感温度: {} °C\n",
+             input.city,
+             (resp.main.temp - 273.15) as i32,
+             (resp.main.temp_max -273.15) as i32,
+             (resp.main.temp_min - 273.15) as i32,
+             resp.main.humidity,
+             resp.main.pressure,
+             (resp.main.feels_like - 273.15) as i32
+    );
     Ok(())
 }
 
 
-// TODO: 1、汉字转拼音
-// TODO: 2、绝对温度转摄氏度
-// TODO: 3、天气预报
 
 
